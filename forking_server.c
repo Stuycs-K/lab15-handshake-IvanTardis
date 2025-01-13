18f821295171d5d4f3955bc5511f93a5782bc034
Ivan Gontchar <ivan.tardis@outlook.com>
2025-01-13 00:11:04 -0500

    i think i adjusted makefile to new specifications, and made forking file, with code from persistant server

---
+++ forking_server.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <dirent.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include "pipe_networking.h"
+
+int to_client;
+int from_client;
+
+int main() {
+  signal(SIGINT, sighandler);
+  signal(SIGPIPE, sighandler);
+
+  //printf("Beginning random number generation\n");
+  while(1){
+    from_client = server_handshake( &to_client );
+    printf("Connected with a client!\n");
+    //int x = open("/dev/random", O_RDONLY, 0300);
+    //int rand;
+    //read(x, &rand, sizeof(int));
+    int randNum = abs(getRand()) % 101;
+    while(1){
+      //printf("Looping again, so this should be right before a new random number.\n");
+      randNum = abs(getRand()) % 101;
+      //read(x, &rand, sizeof(int));
+      //printf("Right after random number, and it is... %d\n", randNum);
+      if(write(to_client, &randNum, sizeof(int)) == -1){
+        printf("Error writing to client\n");
+        close(to_client);
+        break;
+      }
+      //printf("Wrote %d to client\n", randNum);
+      sleep(1);
+    }
+  }
+}
+
+
+static void sighandler(int signo){
+  if ( signo == SIGINT ){
+    printf("Exiting due to SIGINT\n");
+    close(from_client);
+    close(to_client);
+    exit(0);
+  }
+  if ( signo == SIGPIPE ){
+    printf("Caught a SIGPIPE so a client disconnected\n");
+    close(from_client);
+    close(to_client);
+  }
+}
--- makefile
+++ makefile
@@ -1,17 +1,19 @@
-all: client server
-client: persistant_client.o pipe_networking.o
-	gcc -o client persistant_client.o pipe_networking.o
-server: persistant_server.o pipe_networking.o
-	gcc -o server persistant_server.o pipe_networking.o
-basic_client.o: persistant_client.c pipe_networking.h
+compile clientRun serverRun: forking_server.o persistant_client.o pipe_networking.o
+	gcc -o clientRun persistant_client.o pipe_networking.o
+	gcc -o serverRun forking_server.o pipe_networking.o
+client: clientRun
+	./clientRun
+server: serverRun
+	./serverRun
+persistant_client.o: persistant_client.c pipe_networking.h
 	gcc -c persistant_client.c
-persistant_server.o: persistant_server.c pipe_networking.h
-	gcc -c persistant_server.c
+forking_server.o: forking_server.c pipe_networking.h
+	gcc -c forking_server.c
 pipe_networking.o: pipe_networking.c pipe_networking.h
 	gcc -c pipe_networking.c
 clean:
 	rm -f mario
 	rm -f *.o
 	rm -f *~
-	rm -rf client
-	rm -rf server
+	rm -rf clientRun
+	rm -rf serverRun
